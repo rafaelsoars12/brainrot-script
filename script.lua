06651e49343b69a284a1e713db856c8c30b7c929
rafaelsoars12 <168869999+rafaelsoars12@users.noreply.github.com>
2025-09-03 11:14:13 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,19 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+ScreenGui.Parent = player:WaitForChild("PlayerGui")
+
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(1, 0, 1, 0)
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0.3
+
+TextLabel.Parent = Frame
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Text = "Você foi baitado hohoho"
+TextLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.Font = Enum.Font.SourceSansBold
